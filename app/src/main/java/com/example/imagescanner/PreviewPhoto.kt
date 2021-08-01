6d5bad416eb1beb804e985ea57f458a5786e9891
sgkevreki <sgkevreki@inf.uth.gr>
2021-08-01 21:06:11 +0300

    pdf sharing works too now!

--- app/src/main/java/com/example/imagescanner/PreviewPhoto.kt
+++ app/src/main/java/com/example/imagescanner/PreviewPhoto.kt
@@ -2,26 +2,33 @@ package com.example.imagescanner
 
 import android.app.AlertDialog
 import android.app.PendingIntent.getActivity
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.os.Environment
+import android.net.Uri
+import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
+
+import android.graphics.Color
+import android.graphics.Paint
+
+import android.graphics.pdf.PdfDocument
+import android.graphics.pdf.PdfDocument.PageInfo
+import android.os.Environment
+import android.util.Log
 import androidx.core.content.FileProvider
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
+import android.graphics.drawable.BitmapDrawable
+
+
+
 
 
 class PreviewPhoto : AppCompatActivity() {
@@ -73,22 +80,78 @@ class PreviewPhoto : AppCompatActivity() {
         startActivity(Intent.createChooser(intent, "Share Image"))
     }
 
+
+
+    var pdfDocument // to create .pdf file
+            : PdfDocument? = null
+
     private fun sharePDF(view: View) {
+
+
+
+        val bitmap: Bitmap = loadBitmapFromView(view)!!
+
+        pdfDocument = PdfDocument()
+        val pageInfo = PageInfo.Builder(bitmap.width, bitmap.height, 1).create()
+        val page: PdfDocument.Page = pdfDocument!!.startPage(pageInfo)
+
+        val canvas = page.canvas
+        val paint = Paint()
+        paint.color = Color.parseColor("#FFFFFF")
+        canvas.drawBitmap(bitmap, 0.toFloat(), 0.toFloat(), null)
+        pdfDocument!!.finishPage(page)
+
+        if (pdfDocument == null) {
+            Log.i("local-dev", "pdfDocument in 'saveFile' function is null")
+            return
+        }
+
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
+//        var isDirectoryCreated: Boolean = root.exists()
+//        if (!isDirectoryCreated) {
+//            isDirectoryCreated = root.mkdir()
+//        }
+
+        val fileName = "ImageScanner.pdf"
+        val file = File(storageDir, fileName)
+        try {
+            val fileOutputStream = FileOutputStream(file)
+            pdfDocument!!.writeTo(fileOutputStream)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        pdfDocument!!.close()
+
+        val contentUri = FileProvider.getUriForFile(applicationContext,"com.example.android.fileprovider",file)
+
+        Log.i("local-dev", "'saveFile' function successfully done")
+        Toast.makeText(
+            this, """
+ Successful! PATH:
+ Internal Storage/${Environment.DIRECTORY_DOWNLOADS}
+ """.trimIndent(), Toast.LENGTH_SHORT
+        ).show()
+
+//        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+//        intent.type = "application/pdf"
+//        intent.addCategory(Intent.CATEGORY_OPENABLE)
+//        startActivityForResult(intent, 123)
 //        val mBitmap: Bitmap = loadBitmapFromView(view)!!
 //
 //        val path: String =
 //            MediaStore.Images.Media.insertImage(contentResolver, mBitmap, "Image Description", null)
 //        val uri = Uri.parse(path)
 //
-//        val shareIntent = Intent(Intent.ACTION_SEND)
-//        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-//        shareIntent.type = "application/pdf"
-//        /** set the corresponding mime type of the file to be shared */
-//        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
-//
-//        startActivity(Intent.createChooser(shareIntent, "Share the file..."))
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        shareIntent.type = "application/pdf"
+        /** set the corresponding mime type of the file to be shared */
+        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri)
+
+        startActivity(Intent.createChooser(shareIntent, "Share the file..."))
 
     }
+
     private fun loadBitmapFromView(view: View): Bitmap? {
 
         // width measure spec
